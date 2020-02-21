729de666cf86e8bed0476ddd57d1915b9c2e5101
vagrant <vagrant@homestead>
2020-02-21 00:57:36 +0000

    Episode4 - Laracasts Forum Series

--- tests/Feature/ThreadsTest.php
+++ tests/Feature/ThreadsTest.php
@@ -11,9 +11,16 @@ class ThreadsTest extends TestCase
     use DatabaseMigrations;
     /** @test */
     public function a_user_can_browse_threads()
-    {
-        $response = $this->get('/threads');
 
-        $response->assertStatus(200);
+    {
+        $thread = factory('App/Thread')->create();
+
+        $response = $this->get('/threads');
+        $response->assertSee($thread->title);
+
+        $response = $this->get('/threads/' . $thread->id);
+        $response->assertSee($thread->title);
+
+
     }
 }
