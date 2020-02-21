b67dc007e761b666e9ad0799d5ff7e97a21709f3
vagrant <vagrant@homestead>
2020-02-21 21:51:13 +0000

    Episode4 test 2 for viewing single thread - Laracasts Forum Series

--- tests/Feature/ThreadsTest.php
+++ tests/Feature/ThreadsTest.php
@@ -10,7 +10,7 @@ class ThreadsTest extends TestCase
     
     use DatabaseMigrations;
     /** @test */
-    public function a_user_can_browse_threads()
+    public function a_user_can_browse_all_threads()
 
     {
         $thread = factory('App/Thread')->create();
@@ -23,4 +23,14 @@ class ThreadsTest extends TestCase
 
 
     }
+
+    /** @test */
+    function a_user_can_read_a_single_thread()
+    {
+        $thread = factory('App/Thread')->create();
+
+        $response = $this->get('/threads');
+        $response->assertSee($thread->title);
+    }
 }
+
